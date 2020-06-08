9661947562d4bf3e87828650bd48e4c11b396550
mrkihiki <56691684+mrkihiki@users.noreply.github.com>
2020-06-09 01:46:11 +0700

    h

--- lab5_1/lab5_1/lab5_1.cpp
+++ lab5_1/lab5_1/lab5_1.cpp
@@ -1,11 +1,105 @@
 ﻿// lab5_1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 //
 
+#include <math.h>
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <stdio.h>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+
+using namespace std;
+
+struct spik {
+	float s = 0;
+	string imya = " ";
+	string fam = " ";
+	string otch = " ";
+	string raion = " ";
+	int stoimost = 0;
+
+};
+
+bool sor(const spik& a1, const spik& a2)
+{
+	return a1.fam < a2.fam;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	setlocale(LC_ALL, "rus");
+
+	string x;
+	spik s;
+	ifstream vod;
+
+	vod.open("1.txt");
+	int l=0;
+
+	if (vod.is_open()) {
+		while (!vod.eof()) {
+			string s;
+			getline(vod, s);
+			l++;
+		}
+	}
+
+	spik* Arr = new spik[l];
+	vod.seekg(0, ios_base::beg);//начало файла
+
+	for (size_t i = 0; i < l; i++)
+	{
+		getline(vod, x);
+		istringstream iss(x);//поток ввода
+		iss >> s.s >> s.imya >> s.fam >> s.otch >> s.raion >> s.stoimost;
+		Arr[i] = s;
+
+	}
+
+	cout << "Все квартиры: \n" << endl;
+
+	for (size_t n = 0; n < l; n++)
+	{
+		cout << "Площадь : " << Arr[n].s << " "
+			<< "Имя : " << Arr[n].imya << " "
+			<< "Фамилия : " << Arr[n].fam << " "
+			<< "Отчество : " << Arr[n].otch << " "
+			<< "Район : " << Arr[n].raion << " "
+			<< "Стоимость : " << Arr[n].stoimost << endl;
+	}
+
+	cout << "\nКвартиры находящиeся в Центральном районе :\n" << endl;
+
+	for (size_t n = 0; n < l; n++)
+	{
+		if (Arr[n].raion == "central")
+		{
+			cout << "Площадь : " << Arr[n].s << " "
+				<< "Имя : " << Arr[n].imya << " "
+				<< "Фамилия : " << Arr[n].fam << " "
+				<< "Отчество : " << Arr[n].otch << " "
+				<< "Район : " << Arr[n].raion << " "
+				<< "Стоимость : " << Arr[n].stoimost << endl;
+		}
+	}
+	cout << "\nКвартиры стоимостью более 1000к отсортированная по алфавиту:\n" << endl;
+
+	sort(Arr, Arr + l, sor);
+	for (size_t n = 0; n < l; n++)
+	{
+		if (Arr[n].stoimost > 1000)
+		{
+			cout << "Площадь : " << Arr[n].s << " "
+				<< "Имя : " << Arr[n].imya << " "
+				<< "Фамилия : " << Arr[n].fam << " "
+				<< "Отчество : " << Arr[n].otch << " "
+				<< "Район : " << Arr[n].raion << " "
+				<< "Стоимость : " << Arr[n].stoimost << endl;
+		}
+	}
+	vod.close();
 }
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
@@ -18,3 +112,4 @@ int main()
 //   4. В окне "Список ошибок" можно просматривать ошибки.
 //   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
 //   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
+//https://ru.cppreference.com/w/cpp/algorithm/sort
